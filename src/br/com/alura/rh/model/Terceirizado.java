2fdbf4fb789b23585872715a32bb006a0d3a18d5
Felipe Martinez Albeche <felipealbeche@hotmail.com>
2023-08-20 12:10:10 -0300

    add composition for employees common data

--- src/br/com/alura/rh/model/Terceirizado.java
+++ src/br/com/alura/rh/model/Terceirizado.java
@@ -1,22 +1,24 @@
 package br.com.alura.rh.model;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
-public class Funcionario {
-
+public class Terceirizado {
+	
+	private String empresa;
 	private DadosPessoais dadosPessoais;
-	private LocalDate dataUltimoReajuste;
 
-	public Funcionario(String nome, String cpf, Cargo cargo, BigDecimal salario) {
-		this.dadosPessoais = new DadosPessoais(nome,cpf, cargo, salario);
+	public Terceirizado(String nome, String cpf, Cargo cargo, BigDecimal salario) {
+		this.dadosPessoais = new DadosPessoais(nome, cpf, cargo, salario);
 	}
 
-	public void atualizarSalario(BigDecimal novoSalario) {
-		this.dadosPessoais.setSalario(novoSalario);
-		this.dataUltimoReajuste = LocalDate.now();
+	public String getEmpresa() {
+		return empresa;
 	}
 
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+	
 	public String getNome() {
 		return this.dadosPessoais.getNome();
 	}
@@ -48,19 +50,4 @@ public class Funcionario {
 	public void setSalario(BigDecimal salario) {
 		this.dadosPessoais.setSalario(salario);
 	}
-
-	public LocalDate getDataUltimoReajuste() {
-		return dataUltimoReajuste;
-	}
-
-	public void setDataUltimoReajuste(LocalDate dataUltimoReajuste) {
-		this.dataUltimoReajuste = dataUltimoReajuste;
-	}
-
-	public void promover(Cargo novoCargo) {
-		this.dadosPessoais.setCargo(novoCargo);
-	}
-
-	
-
 }
